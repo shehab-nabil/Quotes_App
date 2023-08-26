aa036255967468067edf11ffd1f5a458cae9b145
shehab nabil <shehabnabil790@gmail.com>
2023-08-27 00:13:20 +0300

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class QuoteApp extends StatelessWidget {
+  const QuoteApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Quote App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
