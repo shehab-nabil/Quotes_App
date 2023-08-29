09717638d521da32f81481127c67244ab39aaa3c
shehab nabil <shehabnabil790@gmail.com>
2023-08-30 00:20:16 +0300

    add splash screen

--- lib/config/routes/routes.dart
+++ lib/config/routes/routes.dart
@@ -2,18 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quotes_app/features/random_quote/presentation/cubit/random_quote_cubit.dart';
 import 'package:quotes_app/features/random_quote/presentation/screen/quote_screen.dart';
+import 'package:quotes_app/features/splash/presentation/screen/splash_screen.dart';
 
 import '../../injection_container.dart';
 
 class Routes {
   static const String initialRoute = '/';
-  static const String favoriteQuoteRoute = '/favoriteQuote';
+  static const String randomQuoteRoute = '/randomQoute';
 }
 
 class AppRoutes {
   static Route? onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case Routes.initialRoute:
+        return MaterialPageRoute(builder: (context) => const SplashScreen());
+      case Routes.randomQuoteRoute:
         return MaterialPageRoute(
             builder: (context) => BlocProvider(
                   create: (context) => sl<RandomQuoteCubit>(),
--- lib/features/splash/presentation/screen/splash_screen.dart
+++ lib/features/splash/presentation/screen/splash_screen.dart
@@ -1,4 +1,10 @@
-import 'package:flutter/widgets.dart';
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:quotes_app/core/utils/app_colors.dart';
+import 'package:quotes_app/core/utils/assets_manager.dart';
+
+import '../../../../config/routes/routes.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -8,8 +14,32 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  late Timer _timer;
+
+  _startDelay() {
+    _timer = Timer(const Duration(milliseconds: 2000),
+        () => Navigator.pushReplacementNamed(context, Routes.randomQuoteRoute));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _startDelay();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return;
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+      body: Center(
+        child: Image.asset(ImageAssets.quote),
+      ),
+    );
   }
 }
